a52d735cbf14da90028da94fb52f8277d67ad3bd
shuaibibobo <2441173284@qq.com>
2023-04-10 00:09:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '12',
 });
 
 const openai = new OpenAIApi(configuration);
